259e5c8fda08b71f49b026f6d2bfd04a20e75cc0
crespodylan <crespo.dylan@gmail.fr>
2014-11-07 14:50:31 +0100

    Ajout de la console

---
+++ Client/Windows/Debug/Console.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace Client.Windows.Debug
+{
+    /// <summary>
+    /// Logique d'interaction pour Console.xaml
+    /// </summary>
+    public partial class Console : Window
+    {
+        private static Console singleton = new Console();
+        private static Semaphore verroux = new Semaphore(0,1);
+
+        private Console()
+        {
+            InitializeComponent();
+        }
+
+        public static Console GetInstance()
+        {
+            return singleton;
+        }
+
+        public void Write(string text)
+        {
+            Paragraph p = new Paragraph();
+            p.Inlines.Add(text);
+            Out.Document.Blocks.Add(p);
+        }
+
+        public void WriteLine(string text)
+        {
+            Paragraph p = new Paragraph();
+            p.Inlines.Add(text+Environment.NewLine);
+            Out.Document.Blocks.Add(p);
+        }
+    }
+}
